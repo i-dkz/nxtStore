c7b5d310538772fbb96bd4311dd9f44ccbf5c052
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-11 15:53:16 -0800

    added OrderSummary component

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,10 @@
-
+import { OrderSummary } from "@/components/OrderSummary";
 
 const Cart = () => {
   return (
-    <div className="flex justify-center w-full">
+    <div className="flex flex-col items-center justify-center w-full">
         <h1 className="flex w-[90%] max-w-[1080px] text-4xl font-bold">Your Cart</h1>
+        <OrderSummary />
     </div>
     
   )
