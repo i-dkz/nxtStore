4027a95fb12671a99fc532c671df4d6d7e933af4
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-11 00:11:30 -0800

    fix: searchbar was maxed at 1060px on big screens

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,12 +3,12 @@ import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = () => {
   return (
-    <div className="flex relative w-full">
+    <div className="relative flex w-full">
       <input
         type="text"
-        className="border max-w-[1060px] w-full rounded-md pl-10 pr-4 py-2"
+        className="w-full py-2 pl-10 pr-4 border rounded-md"
       />
-      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <FaSearch className="text-gray-500" />
       </div>
       <button className='ml-2 h-[42px] border rounded-md px-2 hover:bg-slate-100'>Search</button>
