934ee9732d6244e002a30c11de301ba9167a424d
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-11 20:18:12 -0800

    made cart responsive

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,21 +2,29 @@
 import { useCartStore } from "@/store/CartStore";
 import CartItem from "@/components/CartItem";
 import { OrderSummary } from "@/components/OrderSummary";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
   const { cart } = useCartStore();
+  const [subtotal, setSubtotal] = useState(0);
 
+  useEffect(() => {
+    // Calculate subtotal based on the quantity of each item in the cart
+    const newSubtotal = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+    setSubtotal(newSubtotal);
+  }, [cart]);
 
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <h1 className="flex w-[90%] max-w-[1080px] text-4xl font-bold mb-4">
         Your Cart
       </h1>
-      <div className="flex gap-4">
-        <div className="flex flex-col gap-4">
+      <div className="flex flex-wrap justify-center w-full gap-4">
+        <div className="flex flex-col gap-4 w-[90%] max-w-[670px]">
           {cart.map((item, index) => (
             <CartItem
-              key={item.id}
+              key={index}
+              id={item.id}
               title={item.title}
               thumbnail={item.thumbnail}
               price={item.price}
@@ -25,7 +33,7 @@ const Cart = () => {
             />
           ))}
         </div>
-        <OrderSummary subtotal={90} />
+        <OrderSummary subtotal={subtotal} />
       </div>
     </div>
   );
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
+
 interface Props {
+  id: number;
   title: string;
   thumbnail: string;
   price: number;
@@ -6,23 +9,24 @@ interface Props {
   quantity: number;
 }
 
-const CartItem = ({ title, thumbnail, price, category, quantity }: Props) => {
+const CartItem = ({ id, title, thumbnail, price, category, quantity }: Props) => {
   const subtotal = quantity * price;
-  
+
   return (
-    <div className="w-[670px] h-[180px] border rounded-md p-4">
-      <div className="flex gap-4">
-        <img src={thumbnail} className="w-[150px] h-[150px] rounded-md" />
+    <div className="flex h-[180px] border rounded-md p-4">
+      <div className="flex w-full gap-4">
+        <Link href={`../details/${id}`} className="flex overflow-hidden border rounded-md w-[150px] h-[100px]">
+          <img src={thumbnail} className="object-cover" />
+        </Link>
         <div className="flex flex-col w-full">
-            <div className="flex justify-between w-full">
-              <h3 className="font-bold">{title}</h3>
-              <span>${subtotal}</span>
-            </div>
-            <span className="text-slate-400">{category}</span>
-            <span>Quantity: {quantity}</span>
+          <div className="flex justify-between w-full">
+            <h3 className="font-bold">{title}</h3>
+            <span>${subtotal}</span>
+          </div>
+          <span className="text-slate-400">{category}</span>
+          <span>Quantity: {quantity}</span>
         </div>
       </div>
-
     </div>
   );
 };
