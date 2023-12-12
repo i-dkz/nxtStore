da7e27115d38399f38e65eafb1c08c647f9fda9a
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-11 23:26:18 -0800

    added qty updaters to cart items

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { CgMathMinus, CgMathPlus } from "react-icons/cg";
+import { useCartStore } from "@/store/CartStore";
 
 interface Props {
   id: number;
@@ -9,13 +12,54 @@ interface Props {
   quantity: number;
 }
 
-const CartItem = ({ id, title, thumbnail, price, category, quantity }: Props) => {
+const CartItem = ({
+  id,
+  title,
+  thumbnail,
+  price,
+  category,
+  quantity,
+}: Props) => {
   const subtotal = quantity * price;
 
+  const { addToCart, removeFromCart, clearCart } = useCartStore();
+
+  const handleAddToCart = () => {
+    // Call the addToCart function to update quantity
+    addToCart({
+      id,
+      title,
+      thumbnail,
+      price,
+      category,
+      quantity: 1, // Increment quantity
+    });
+  };
+
+  const handleSubtractFromCart = () => {
+    // Call the addToCart function to update quantity
+    if (quantity <= 1) {
+      removeFromCart(id);
+      return;
+    }
+    
+    addToCart({
+      id,
+      title,
+      thumbnail,
+      price,
+      category,
+      quantity: -1, // Increment quantity
+    });
+  };
+
   return (
-    <div className="flex h-[130px] border rounded-md p-4">
+    <div className="flex h-[140px] border rounded-md p-4">
       <div className="flex w-full gap-4">
-        <Link href={`../details/${id}`} className="flex overflow-hidden border rounded-md w-[150px] h-[100px]">
+        <Link
+          href={`../details/${id}`}
+          className="flex overflow-hidden border rounded-md w-[150px] h-[100px]"
+        >
           <img src={thumbnail} className="object-cover" />
         </Link>
         <div className="flex flex-col w-full">
@@ -25,6 +69,22 @@ const CartItem = ({ id, title, thumbnail, price, category, quantity }: Props) =>
           </div>
           <span className="text-slate-400">{category}</span>
           <span>Quantity: {quantity}</span>
+          <div className="flex gap-2">
+            <Button
+              className="transition-transform transform z-5 duration-10 focus:outline-none active:scale-90"
+              variant={"outline"}
+              onClick={handleAddToCart}
+            >
+              <CgMathPlus size={20} />
+            </Button>
+            <Button
+              className="transition-transform transform duration-10 focus:outline-none active:scale-90"
+              variant={"outline"}
+              onClick={handleSubtractFromCart}
+            >
+              <CgMathMinus />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
