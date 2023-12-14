4478e649ef815bb63f91f719c2f597e5c9c3d584
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-13 18:24:12 -0800

    stylechange to cart icon

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -8,11 +8,11 @@ const CartIcon = ({ qty }: Props) => {
   return (
     <div className="flex relative items-center justify-center w-[45px] h-[45px]">
       {qty === 0 ? undefined : qty > 99 ? (
-        <div className="absolute bg-red-600 rounded-full w-[22px] h-[22px] right-0 top-0 text-white flex text-[0.625rem] justify-center items-center">
+        <div className="absolute bg-red-600 rounded-full w-[22px] h-[22px] right-0 top-0 text-white flex text-[0.625rem] justify-center items-center font-bold">
           99+
         </div>
       ) : (
-        <div className="absolute bg-red-600 rounded-full w-[22px] h-[22px] right-0 top-0 text-white flex text-[0.625rem] justify-center items-center">
+        <div className="absolute bg-red-600 rounded-full w-[22px] h-[22px] right-0 top-0 text-white flex text-[0.625rem] justify-center items-center font-bold">
           {qty}
         </div>
       )}
