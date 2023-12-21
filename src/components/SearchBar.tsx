92e2a46cea70a053fcaa8bd2d33cda20775abc74
Zach Flentge-Wong <zachflentgewong@gmail.com>
2023-12-20 21:24:56 -0800

    created search results page

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Add your logic for handling the search here
+    console.log(event)
+  };
+
   return (
-    <div className="relative flex w-full">
+    <form onSubmit={handleSubmit} className="relative flex w-full">
       <input
         type="text"
         className="w-full py-2 pl-10 pr-4 border rounded-md"
@@ -11,8 +17,8 @@ const SearchBar = () => {
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <FaSearch className="text-gray-500" />
       </div>
-      <button className='ml-2 h-[42px] border rounded-md px-2 hover:bg-slate-100'>Search</button>
-    </div>
+      <button className='ml-2 h-[42px] border rounded-md px-2 hover:bg-slate-100' type='submit'>Search</button>
+    </form>
   );
 };
 
